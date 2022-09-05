ed075d8a4339e82cd25de57c2e20736bad04900a
alexandr <example@gmail.com>
2022-09-05 14:26:21 +0300

    script adp mod

--- Main project css1/js/srcript.js
+++ Main project css1/js/srcript.js
@@ -2,6 +2,22 @@
 // UPPER_SNAKE_CASE
 // kebab-case
 // PascalCase
-const userName = 'John';
-let userNumber = 25;
-userNumber = 24;
+// const userName = 'John';
+// let userNumber = 25;
+// userNumber = 24;
+const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+
+const personalMovieDB = {
+      count:numberOfFilms,
+      movies:{},
+      actors:{},
+      genres:[],
+      privat:false
+};
+
+const a = prompt("Какой последний фильм вы посмотрели?", ""),
+      b = prompt("Как вы его оцените?", ""),
+      c = prompt("Какой последний фильм вы посмотрели?", ""),
+      d = prompt("Как вы его оцените?", "");
+
+ personalMovieDB.movies[a] = b;
